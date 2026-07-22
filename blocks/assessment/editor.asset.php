4ae316ac887b15a6538df9b6b1a2b5e80a472580
Asfand Yar <76555129+ayk-crypto@users.noreply.github.com>
2026-07-22 12:54:59 +0500

    Update blocks/assessment/editor.asset.php for 0.17.0-alpha.1

--- blocks/assessment/editor.asset.php
+++ blocks/assessment/editor.asset.php
@@ -1,5 +1,5 @@
 <?php
 return array(
 	'dependencies' => array( 'wp-blocks', 'wp-block-editor', 'wp-components', 'wp-data', 'wp-element', 'wp-server-side-render' ),
-	'version'      => '0.15.0-alpha.2',
+	'version'      => '0.17.0-alpha.1',
 );
